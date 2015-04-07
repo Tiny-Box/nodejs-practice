977816ea04d4ca18507ddaf0532fca99e13032de
Tiny-Box <Tiny_Box@outlook.com>
2015-04-07 11:10:08 -0400

    callback to deal with block

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function') {
-		return handle[pathname]();
+		handle[pathname](response);
 	} else {
 		console.log("No request handler found for " + pathname);
-		return "I'm sorry, it's missing.";
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("I'm sorry, it's missing.");
+		response.end();
 	}
 }
 
