a80309b3bf3a75670655229f291725a4ff0e91de
Tiny-Box <Tiny_Box@outlook.com>
2015-04-08 12:43:53 -0400

    fix bug(input error)

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,23 @@
 var querystring = require("querystring");
+var sys = require("sys");
 var fs = require("fs");
 var formidable = require("formidable");
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
 
-	var body = '<html>\n' + 
-		'<head>\n'+
-		'<meta http-equiv="Content-Type" content="text/html;\n' +
-		'<charset=UTF-8" />\n' + 
-		'</head>\n' +
-		'<body>\n' + 
-		'<form action="/upload" enctype="multipart/form-data">' + 
+	var body = '<html>' + 
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html;' +
+		'charset=UTF-8" />' + 
+		'</head>' +
+		'<body>' + 
+		'<form action="/upload" enctype="multipart/form-data" '+ 
 		'method="post">' + 
-		'<input type="file" name="upload">' + 
-		'<input type="submit" value="Upload file" />\n' +
-		'</form>\n' +
-		'</body>\n' +
+		'<input type="file" name="upload" multiple="multiple">' + 
+		'<input type="submit" value="Upload file" />' +
+		'</form>' +
+		'</body>' +
 		'</html>';		
 
 	response.writeHead(200, {"Content-Type": "text/html"});
@@ -32,9 +33,12 @@ function upload(response, request) {
 	console.log("about to parse");
 	form.parse(request, function(error, fields, files) {
 		console.log("parsing done");
-		console.log(files.upload.path);
-		fs.renameSync(files.upload.path, "/tmp/test.png");
-		response.writeHead(200, {"Content-Type": "text/plain"});
+		try {
+			fs.renameSync(files.upload.path, "tmp/test.png");
+		} catch(e){
+			console.log(e);
+		}
+		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("received image:<br/>");
 		response.write("<img src='/show' />");
 		response.end();
